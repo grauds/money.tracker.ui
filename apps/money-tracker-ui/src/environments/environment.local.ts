dd945e4f9f6758ef5f5386ec57acb37111cbf099
grauds <antroshin@wiley.com>
2022-09-08 19:08:34 +0300

    [-] Fixed build

--- apps/money-tracker-ui/src/environments/environment.local.ts
+++ apps/money-tracker-ui/src/environments/environment.local.ts
@@ -4,5 +4,8 @@ export const environment = {
     username: '111111111-11',
     name: 'Whatever Something de Paula',
     email: 'whatever@gmail.com',
-  }
+  },
+  apiUrl: '/api',
+  // apiUrl: 'http://localhost:8080/api',
+  authUrl: 'http://192.168.1.2:8083/auth/'
 };
--- apps/money-tracker-ui/src/environments/environment.prod.ts
+++ apps/money-tracker-ui/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: '/api',
+  // apiUrl: 'http://localhost:8080/api',
+  authUrl: 'http://192.168.1.2:8083/auth/'
 };
