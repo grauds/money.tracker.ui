58209b2f64748644f5194acfe082a226fa945915
grauds <antroshin@wiley.com>
2023-02-26 15:54:37 +0300

    [+] Added cobertura reporter

--- jest.preset.js
+++ jest.preset.js
@@ -2,5 +2,5 @@ const nxPreset = require('@nrwl/jest/preset').default;
 
 module.exports = {
   ...nxPreset,
-  coverageReporters: ["clover", "json", "lcov", "text"]
+  coverageReporters: ["clover", "json", "lcov", "text", "cobertura", "html"]
 }
