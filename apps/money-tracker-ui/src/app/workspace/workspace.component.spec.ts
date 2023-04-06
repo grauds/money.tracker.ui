1d32da66d085efd5a19ed630707d1b65bd886d31
grauds <antroshin@wiley.com>
2023-04-06 19:41:52 +0300

    [*] Fixed tests

--- apps/money-tracker-ui/src/app/workspace/workspace.component.spec.ts
+++ apps/money-tracker-ui/src/app/workspace/workspace.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WorkspaceComponent } from './workspace.component';
+import { KeycloakService } from "keycloak-angular";
+import { ActivatedRoute, convertToParamMap } from "@angular/router";
 
 describe('WorkspaceComponent', () => {
   let component: WorkspaceComponent;
   let fixture: ComponentFixture<WorkspaceComponent>;
 
+  const fakeActivatedRoute = {
+    snapshot: { paramMap: convertToParamMap({ 'id': 19}) }
+  } as ActivatedRoute;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [WorkspaceComponent],
+      providers: [
+        KeycloakService,
+        {provide: ActivatedRoute, useValue: fakeActivatedRoute}
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(WorkspaceComponent);
