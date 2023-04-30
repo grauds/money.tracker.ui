74cce1c5b4c105466b2fec6d4c47280991130d85
Anton Troshin <anton.troshin@gmail.com>
2023-04-30 18:07:04 +0300

    [*] Fixed tests

--- libs/shared-components/src/lib/service/money-type.service.spec.ts
+++ libs/shared-components/src/lib/service/money-type.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MoneyTypeService } from './money-type.service';
+import { HttpClient, HttpHandler } from "@angular/common/http";
 
 describe('MoneyTypeService', () => {
   let service: MoneyTypeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [MoneyTypeService, HttpClient, HttpHandler]
+    });
     service = TestBed.inject(MoneyTypeService);
   });
 
