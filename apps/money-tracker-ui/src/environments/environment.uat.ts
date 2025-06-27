efd2bda0301f55147c25d85141103aaddc1dbfa8
Anton Troshin <11165918+grauds@users.noreply.github.com>
2025-06-27 21:02:05 +0300

    [+] Fixed uat and demo Keycloak URLs

--- apps/money-tracker-ui/src/environments/environment.uat.ts
+++ apps/money-tracker-ui/src/environments/environment.uat.ts
@@ -4,5 +4,5 @@ export const environment: EnvironmentInterface = {
   production: false,
   apiUrl: 'http://192.168.1.118:18085/api',
   infoUrl: 'http://192.168.1.118:18085',
-  authUrl: 'http://192.168.1.157:8080/',
+  authUrl: 'https://192.168.1.157:443/',
 };
