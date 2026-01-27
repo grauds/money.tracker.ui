787b0c1c2185b6503ff2cbdfa5e0a97cb4bddaa3
Anton Troshin <11165918+grauds@users.noreply.github.com>
2026-01-27 15:18:10 +0300

    [+] Updated DEMO environment to use HTTPS

--- apps/money-tracker-ui/src/environments/environment.demo.ts
+++ apps/money-tracker-ui/src/environments/environment.demo.ts
@@ -2,7 +2,7 @@ import { EnvironmentInterface } from '@clematis-shared/shared-components';
 
 export const environment: EnvironmentInterface = {
   production: true,
-  apiUrl: 'http://192.168.1.118:18086/api',
-  infoUrl: 'http://192.168.1.118:18086',
+  apiUrl: '/api',
+  infoUrl: '/info',
   authUrl: 'https://192.168.1.157:443/',
 };
